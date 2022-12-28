940e531fd7a589aa032e99480ebebb66ad804d9e
sarif0605 <rivzalchantara13@gmail.com>
2022-12-28 09:36:30 +0700

    Tugas

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import LandingPage from "pages/LandingPage";
+import DetailsPage from "pages/DetailsPage";
+import Checkout from "pages/Checkout";
+import Example from "pages/Example";
+
+import "assets/scss/style.scss";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Route exact path="/" component={LandingPage} />
+        <Route exact path="/properties/:id" component={DetailsPage} />
+        <Route path="/checkout" component={Checkout} />
+        <Route path="/example" component={Example} />
+      </Router>
     </div>
   );
 }
